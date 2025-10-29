7e566110ab18a9bc377be2b39987535d989d884b
Louis Westerheide <louis.westerheide@eccenca.com>
2025-10-29 13:43:27 +0100

    add parameter for choosing output path

--- cmem_plugin_logpoint/search_logs_task.py
+++ cmem_plugin_logpoint/search_logs_task.py
@@ -6,14 +6,19 @@ from typing import Any
 
 import requests
 from cmem_plugin_base.dataintegration.context import ExecutionContext
-from cmem_plugin_base.dataintegration.description import Icon, Plugin, PluginParameter
+from cmem_plugin_base.dataintegration.description import Icon, Plugin, PluginAction, PluginParameter
 from cmem_plugin_base.dataintegration.entity import (
     Entities,
+    Entity,
+    EntityPath,
+    EntitySchema,
 )
+from cmem_plugin_base.dataintegration.parameter.multiline import MultilineStringParameterType
 from cmem_plugin_base.dataintegration.parameter.password import Password, PasswordParameterType
 from cmem_plugin_base.dataintegration.plugins import WorkflowPlugin
 from cmem_plugin_base.dataintegration.ports import (
     FixedNumberOfInputs,
+    FixedSchemaPort,
     UnknownSchemaPort,
 )
 from cmem_plugin_base.dataintegration.types import IntParameterType
@@ -58,6 +63,25 @@ from cmem_plugin_base.dataintegration.utils.entity_builder import build_entities
             param_type=IntParameterType(),
             default_value=1000,
         ),
+        PluginParameter(
+            name="paths_list",
+            label="List of output paths",
+            param_type=MultilineStringParameterType(),
+            default_value="",
+        ),
+    ],
+    actions=[
+        PluginAction(
+            name="preview_output_paths",
+            label="Preview output paths",
+            description="This action lists the potential paths for the query. "
+            "These can be used to specify the needed output schema paths.",
+        ),
+        PluginAction(
+            name="preview_repositories",
+            label="Preview repositories",
+            description="This action lists the potential repositories for the query.",
+        ),
     ],
 )
 class RetrieveLogs(WorkflowPlugin):
@@ -71,6 +95,7 @@ class RetrieveLogs(WorkflowPlugin):
         query: str,
         time_range: str,
         limit: int,
+        paths_list: str,
     ) -> None:
         self.base_url = base_url.removesuffix("/")
         self.account = account
@@ -80,8 +105,13 @@ class RetrieveLogs(WorkflowPlugin):
         if limit < 1:
             raise ValueError("Limit must be positive.")
         self.limit = limit
+        self.paths_list = paths_list
         self.input_ports = FixedNumberOfInputs(ports=[])
-        self.output_port = UnknownSchemaPort()
+        self.output_port = (
+            FixedSchemaPort(schema=self.generate_schema())
+            if self.paths_list
+            else UnknownSchemaPort()
+        )
 
     def execute(
         self,
@@ -93,7 +123,23 @@ class RetrieveLogs(WorkflowPlugin):
             query=self.query, time_range=self.time_range, limit=self.limit, repos=[]
         )
         results = self.search_retrieve_logs(search_id)
-        return build_entities_from_data(results)
+
+        if not self.paths_list:
+            return build_entities_from_data(results)
+
+        # need to adjust entity_uri
+        schema = self.generate_schema()
+        entities = []
+        for result in results:
+            entity_uri = "test"
+            values = []
+            for path in schema.paths:
+                try:
+                    values.append([str(result[path.path])])
+                except KeyError as e:
+                    raise KeyError("The output path list contains invalid paths.") from e
+            entities.append(Entity(uri=entity_uri, values=values))
+        return Entities(entities=iter(entities), schema=schema)
 
     def search_retrieve_logs(self, search_id: str) -> list[dict]:
         """Get search results for a search id"""
@@ -117,7 +163,6 @@ class RetrieveLogs(WorkflowPlugin):
             response.raise_for_status()
 
             response_data = response.json()
-            print(response_data.get("final"))
             rows = response_data["rows"]
             full_response.extend(rows)
             final = response_data.get("final", True)
@@ -142,7 +187,7 @@ class RetrieveLogs(WorkflowPlugin):
         search_id: str = response.json()["search_id"]
         return search_id
 
-    def list_repositories(self) -> None:
+    def list_repositories(self, context: ExecutionContext) -> None:
         """List all repositories available in the logpoint service.
 
         Currently, this is not working as a JSON Web Token (JWT) is needed for this.
@@ -153,5 +198,32 @@ class RetrieveLogs(WorkflowPlugin):
             "username": self.account,
             "secret_key": self.secret_key,
         }
-        headers = {"Content-Type": "application/json"}
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {context.user.token()}",
+        }
         requests.post(url=url, data=data, headers=headers, timeout=100)
+
+    def preview_output_paths(self) -> str:
+        """Preview output paths"""
+        preview_string = ""
+        search_id = self.search_start(
+            query=self.query, time_range=self.time_range, limit=1, repos=[]
+        )
+        results = self.search_retrieve_logs(search_id)
+        result = results[0]
+        for r in result:
+            preview_string += f"- {r}\n"
+        return preview_string
+
+    def preview_repositories(self) -> str:
+        """Preview repositories"""
+        return "Not yet implemented"
+
+    def generate_schema(self) -> EntitySchema:
+        """Generate the specified output schema."""
+        # need to make sure that first of all white spaces get ignored
+        return EntitySchema(
+            type_uri="test",
+            paths=[EntityPath(split_path) for split_path in self.paths_list.split(",")],
+        )
--- tests/test_search_logs_task.py
+++ tests/test_search_logs_task.py
@@ -3,6 +3,7 @@
 from dataclasses import dataclass
 
 import pytest
+from cmem_plugin_base.testing import TestExecutionContext
 
 from cmem_plugin_logpoint.search_logs_task import RetrieveLogs
 from tests.conftest import get_env_or_skip
@@ -31,6 +32,7 @@ def search_environment() -> SearchEnvironment:
         query="",
         time_range="Last 1 hour",
         limit=10000,
+        paths_list="",
     )
     return SearchEnvironment(
         base_url=base_url,
@@ -46,3 +48,36 @@ def test_search(search_environment: SearchEnvironment) -> None:
     query = '"device_name" = "EKSAuditDevice"'
     search_id = plugin.search_start(repos=[], limit=10, time_range="Last 1 hour", query=query)
     plugin.search_retrieve_logs(search_id)
+
+
+def test_list_repos(search_environment: SearchEnvironment) -> None:
+    """Test start search"""
+
+
+def test_preview_output_paths(search_environment: SearchEnvironment) -> None:
+    """Test preview action to show output paths"""
+    plugin = search_environment.plugin
+    plugin.query = "| chart count() by device_ip"
+    plugin.time_range = "Last 15 minutes"
+    plugin.limit = 10
+    assert "count()" in plugin.preview_output_paths()
+
+
+def test_plugin_with_output_specified() -> None:
+    """Test plugin with a specific output specified"""
+    base_url = get_env_or_skip("LOGPOINT_BASE_URL")
+    account = get_env_or_skip("LOGPOINT_ACCOUNT")
+    secret_key = get_env_or_skip("LOGPOINT_SECRET_KEY")
+    plugin = RetrieveLogs(
+        base_url=base_url,
+        account=account,
+        secret_key=secret_key,
+        query="| chart count() by device_ip",
+        time_range="Last 15 minutes",
+        limit=10,
+        paths_list="_type_str,count()",
+    )
+    result = plugin.execute(inputs=[], context=TestExecutionContext())
+
+    assert len(result.schema.paths) == len(plugin.paths_list.split(","))
+    assert len(list(result.entities)) > 0
